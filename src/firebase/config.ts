7a0cb9666cd4d4cbe2abfd201b8149f56791ed32
João Vitor Machry <joaovitormachry@gmail.com>
2025-11-12 21:09:06 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5983444212-e3740",
+  "appId": "1:988851325152:web:85d8d34e9c6f6104bda885",
+  "apiKey": "AIzaSyCgEicWqzqEVGuw1OXlW8YBquemtntc09E",
+  "authDomain": "studio-5983444212-e3740.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "988851325152"
+};
